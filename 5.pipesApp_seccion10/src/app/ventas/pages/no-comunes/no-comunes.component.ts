c1ef464c48878be178df57db5933bca3801a3d95
Patricia Maldonado <patriimaldonado@gmail.com>
2022-12-10 22:00:25 +0100

    finalizada seccion de pipes de angular

--- 5.pipesApp_seccion10/src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ 5.pipesApp_seccion10/src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -30,5 +31,47 @@ export class NoComunesComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  cambiarCliente(){
+    this.nombre = 'Juan';
+    this.genero = 'masculino';
+
+  }
+  borrarCliente(){
+    this.clientes.pop();
+  }
+
+  // keyValue Pipe
+
+  persona = {
+    nombre: 'Patricia',
+    edad:23,
+    direccion:  'flores rojas'
+  }
+
+  heroes = [
+    {
+      nombre: 'spiderman',
+      value: 34
+    },
+    {
+      nombre: 'superman',
+      value: 30
+    },
+    {
+      nombre: 'hulk',
+      value: 56
+    },
+  ]
+
+// Async ie
+// interval imprime valores en el intervalo que especifiques y con el pipe async se van mostrando
+miObservable = interval(5000); // 0,1,2..
+
+valorPromesa = new Promise((resolve,reject) =>{
+  setTimeout(() => {
+    resolve('Tenemos data de promesa')
+  }, 3500);
+});
 
 }
