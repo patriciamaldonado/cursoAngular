77219e00967adf1945548c8122833730dbb2fd23
Patricia Maldonado <patriimaldonado@gmail.com>
2022-12-14 22:04:06 +0100

    Seccion de pipes personalizados

--- 5.pipesApp_seccion10/src/app/ventas/pages/ordenar/ordenar.component.ts
+++ 5.pipesApp_seccion10/src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -7,9 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdenarComponent implements OnInit {
 
+  palabra: string = 'nosotros';
+  enMayusculas: boolean = true;
+  ordenarPor: string = '';
+  heroes: Heroe[] =[
+    {
+    nombre: 'Superman',
+    vuela: true,
+    color: Color.azul
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde
+    },
+    {
+      nombre: 'Dardevil',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Linterna verde',
+      vuela: true,
+      color: Color.rojo
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  mayusMinus(){
+    // if(this.enMayusculas){
+    //   this.enMayusculas = false;
+    // }
+    // else{
+    //   this.enMayusculas = true;
+    // }
+    this.enMayusculas = !this.enMayusculas;
+  }
+
+  cambiarOrden(valor:string){
+    this.ordenarPor = valor;
+  }
+
 
 }
