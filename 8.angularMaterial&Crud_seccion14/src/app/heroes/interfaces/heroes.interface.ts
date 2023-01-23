37ae64294f853b812f599fb7c327ab8cce326209
Patricia Maldonado <patriimaldonado@gmail.com>
2023-01-24 00:11:10 +0100

    arreglo seccion crud

--- 8.angularMaterial&Crud_seccion14/src/app/heroes/interfaces/heroes.interface.ts
+++ 8.angularMaterial&Crud_seccion14/src/app/heroes/interfaces/heroes.interface.ts
@@ -1,11 +1,11 @@
 export interface Heroe {
-    id?:               string;
+    id?:              string;
     superhero:        string;
     publisher:        Publisher;
     alter_ego:        string;
     first_appearance: string;
     characters:       string;
-    alt_img?:         string; //http..path image
+    alt_img?:         string; // https://kasdfjaskdfajsdf.com/img.png
 }
 
 export enum Publisher {
--- 8.angularMaterial&Crud_seccion14/src/app/material/material.module.ts
+++ 8.angularMaterial&Crud_seccion14/src/app/material/material.module.ts
@@ -1,32 +1,36 @@
 import { NgModule } from '@angular/core';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatListModule} from '@angular/material/list';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {MatInputModule} from '@angular/material/input';
 
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog'; 
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    
-  ],
   exports: [
-    MatSidenavModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatListModule,
-    MatCardModule,
-    MatButtonModule,
-    MatProgressSpinnerModule,
-    MatGridListModule,
     MatAutocompleteModule,
-    MatInputModule
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatInputModule,
+    MatListModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSnackBarModule,
+    MatToolbarModule
   ]
 })
 export class MaterialModule { }
