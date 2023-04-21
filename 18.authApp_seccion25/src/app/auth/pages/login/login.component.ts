2716d03990e3c488b7753ab9c66a471af3ce2939
Patricia Maldonado <patriimaldonado@gmail.com>
2023-04-21 11:16:44 +0200

    nuevas secciones añadidas y actualizaciones

---
+++ 18.authApp_seccion25/src/app/auth/pages/login/login.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import Swal from "sweetalert2";
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  miFormulario: FormGroup = this.fb.group({
+    email: ['test1@test.com', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]]
+  })
+
+  constructor(private fb: FormBuilder,
+    private router: Router,
+    private authService: AuthService) { }
+
+ login(){
+
+  console.log(this.miFormulario.value);
+  const {email, password} = this.miFormulario.value;
+
+  this.authService.login(email, password).subscribe(ok =>{
+    console.log(ok);
+    
+    if(ok === true){
+      this.router.navigateByUrl('/dashboard')
+    } else{
+      Swal.fire('Error', ok, 'error');
+
+    }
+    
+  });
+  
+
+ }
+}
